9fe90c2aedbe138af72013f2ef89d3ca03b1d8bf
BM-ACADEMY <teamkamarweb@gmail.com>
2025-10-23 15:57:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const sendMail = require('./nodemailer');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // Middleware
 app.use(cors({
